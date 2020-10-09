edd7061f382d3090cf9988d65fe7e5b627e5de7c
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-10-09 12:10:21 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$("#main").addClass("w3-container w3-flat-peter-river");
+	$("body").addClass("w3-flat-green-sea");
+	$("body").css({
+		"overflow": "hidden",
+		"font-family": "Open Sans, sans-serif"
+	});
+	$(".bar").addClass("w3-bar-block w3-black");
+	$(".bar").css("height", "12px");
+});
