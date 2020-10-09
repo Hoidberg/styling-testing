1d1a8af6f604206d8151804aaffd7d2477578141
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-10-09 12:58:16 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-	w3.addClass("#main", "w3-container w3-flat-peter-river");
-	w3.addClass("body", "w3-flat-green-sea");
+	$("#main").addClass("w3-container w3-flat-peter-river");
+	$("body").addClass("w3-flat-green-sea");
 	$("body").css({
 		"overflow": "hidden",
 		"font-family": "Open Sans, sans-serif"
 	});
-	w3.addClass(".bar", "w3-bar-block w3-black");
+	$(".bar").addClass("w3-bar-block w3-black");
 	$(".bar").css("height", "12px");
 });
