8521e92abee0eddc040282f35abc30ac5ec1c241
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-10-12 17:38:41 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,21 +12,14 @@ const RenderMain = {
 	data() {
 		return {
 			cardTitle: '<p>This is a card</p>',
-			cardDesc: '<p>A card is a container for any HTML content</p>'
-		}
-	}
-}
-const RenderSymbols = {
-	data() {
-		return {
-			third: '<h3 class="w3-center">&#8531;</h3>',
+			cardDesc: '<p>A card is a container for any HTML content</p>',
+			third: '<h3 class="w3-center">&#8531;</h3>'
 		}
 	}
 }
 
 Vue.createApp(RenderHeader).mount('#header')
 Vue.createApp(RenderMain).mount('#main')
-Vue.createApp(RenderSymbols).mount('.symbol')
 
 $(document).ready(function(){
 	$("#main").addClass("w3-container w3-flat-peter-river");
