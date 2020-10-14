7036315b700a1f7b5122c86689948db624237454
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-10-14 14:30:27 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,7 +22,9 @@ Vue.createApp(RenderHeader).mount('#header')
 Vue.createApp(RenderMain).mount('#main')
 
 $(document).ready(function(){
-	$("#code1").load("https://raw.githubusercontent.com/Hoidberg/styling-testing/main/js/script.js");
+	$.get("https://hoidberg.github.io/styling-testing/js/script.js", function(data, status){
+		$("#code1").text(data);
+	});
 	$("#main").addClass("w3-container w3-flat-peter-river");
 	$("body").addClass("w3-flat-green-sea iconsize1 fontsize1");
 	$("body").css({"overflow": "hidden", "font-family": "Open Sans, sans-serif"});
