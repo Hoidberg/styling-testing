aea39c6156d61a2e51292c2144fde1abaa3831e4
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-10-17 17:16:45 -0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+require.config({
+	baseUrl: "/styling-testing/resources",
+	paths: {
+		jquery: 'jQuery/jquery.min.js',
+		vuejs: 'VueJS/vue.js',
+		modernizr: 'modernizr/modernizr.min.js',
+	},
+	waitSeconds: 30
+});
