b6b9770a8ac9b382cab23372a5700d8d00c3dd3a
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-10-18 10:04:51 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ require.config({
 	baseUrl: "/styling-testing/resources",
 	paths: {
 		jquery: 'jQuery/jquery.min.js',
+		jqueryui: 'jQueryUI/jquery-ui.min.js',
 		vuejs: 'VueJS/vue.js',
 		modernizr: 'modernizr/modernizr.min.js',
 	},
