2e642b445849b6a9dc8557d89a6a1f1b3184f689
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-10-18 11:07:17 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,5 +25,5 @@ Vue.createApp(RenderMain).mount('#main')
 
 j("#main").addClass("w3-container w3-flat-peter-river");
 j("body").addClass("w3-flat-green-sea iconsize1 fontsize1");
-j("body").css({"overflow": "hidden", "font-family": "Open Sans, sans-serif"});
+j("body").css({"overflow": "hidden", "font-family": "Open Sans, sans-serif", "background-image": "url('https://i.pinimg.com/originals/27/18/2f/27182f4f61a041bfc1e6af79821316a6.jpg')", "background-repeat": "repeat", "background-blend-mode": "overlay"});
 j("link[rel='icon']").addClass("icon");
