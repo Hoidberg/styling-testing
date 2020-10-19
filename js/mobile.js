8d2c465b3f033132be0f7cf3429437459c167da1
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-10-19 14:29:46 -0500

    Create mobile.js

---
+++ js/mobile.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+	for (;;){
+		setTimeout(function () {
+			if (Modernizr.mq('only all and (max-width: 480px)')) {
+				if (j("body").hasClass("w3-mobile") == false) {
+					j("body").addClass("w3-mobile");
+				}
+			} else {
+				if (j("body").hasClass("w3-mobile") == true) {
+					j("body").removeClass("w3-mobile");
+				}
+			}
+		}, 250);
+	}
+}
