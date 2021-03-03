6847f67780965a9213ad4f15a02aafc1f5f26f6b
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2021-03-03 13:00:41 -0600

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -1,12 +1,14 @@
+var j = $.noConflict();
+
 window.onload = function() {
 	var interval = setInterval(function() {
 		if (Modernizr.mq('only all and (max-width: 480px)')) {
-			if ($("body").hasClass("w3-mobile") == false) {
-				$("body").addClass("w3-mobile");
+			if (j("body").hasClass("w3-mobile") == false) {
+				j("body").addClass("w3-mobile");
 			}
 		} else {
-			if ($("body").hasClass("w3-mobile") == true) {
-				$("body").removeClass("w3-mobile");
+			if (j("body").hasClass("w3-mobile") == true) {
+				j("body").removeClass("w3-mobile");
 			}
 		}
 	}, 250);
